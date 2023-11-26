6fab8f890899e12f5ac711ac5d2d3d09aedaf4b3
Prasann Agrawal <prasann25@gmail.com>
2023-11-26 01:45:49 -0800

    Section 23 Feature Modules changes

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { IngredientComponent } from "./ingredient/ingredient.component";
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListComponent } from "./shopping-list.component";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+    declarations : [
+        ShoppingListComponent,
+        IngredientComponent,
+        ShoppingEditComponent,
+    ],
+    imports : [
+        FormsModule,
+        RouterModule.forChild([
+            {path: 'shopping-list', component: ShoppingListComponent}
+        ]),
+        SharedModule
+    ]
+})
+export class ShoppingListModule {
+
+}
